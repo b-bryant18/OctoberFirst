0a6d80739eba0b174a5dceb58e21dfe9f92f4d9c
Brad Bryant <bradleybryant86@gmail.com>
2020-10-06 00:17:05 -0500

    Created User database model in User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,14 @@
 const mongoose = require('mongoose');
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+mongoose.model('User', UserSchema);
